808e165cdd2aee5938788a8b9faddaa2c065086e
Papp Zsolt <pappzsolt3@gmail.com>
2018-09-27 04:15:34 +0200

    admin things

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -3,10 +3,24 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { AdminShellComponent } from './admin-shell/admin-shell.component';
 import { ActorComponent } from './actor/actor.component';
+import { AgeClassificationComponent } from './age-classification/age-classification.component';
+import { EpisodeComponent } from './episode/episode.component';
+import { GenderComponent } from './gender/gender.component';
+import { GenreComponent } from './genre/genre.component';
+import { MovieComponent } from './movie/movie.component';
+import { SeasonComponent } from './season/season.component';
+import { SeriesComponent } from './series/series.component';
 
 const adminRoutes: Routes = [
   { path: '', component: AdminShellComponent },
-  { path: 'actor', component: ActorComponent }
+  { path: 'actor', component: ActorComponent },
+  { path: 'age-classification', component: AgeClassificationComponent },
+  { path: 'episode', component: EpisodeComponent },
+  { path: 'gender', component: GenderComponent },
+  { path: 'genre', component: GenreComponent },
+  { path: 'movie', component: MovieComponent },
+  { path: 'season', component: SeasonComponent },
+  { path: 'series', component: SeriesComponent }
 ];
 
 @NgModule({
