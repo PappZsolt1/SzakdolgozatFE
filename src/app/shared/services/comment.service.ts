c41024ecc6d7f1d33184b069ec58439c0d30e1cc
Papp Zsolt <pappzsolt3@gmail.com>
2018-10-04 18:47:05 +0200

    services

--- src/app/shared/services/comment.service.ts
+++ src/app/shared/services/comment.service.ts
@@ -1,8 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import { Comment } from '../models/comment.model';
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
 
 @Injectable()
 export class CommentService {
 
-  constructor() { }
+  commentUrl = "http://localhost:8080/comment";
 
+  constructor(private http: HttpClient) { }
+
+  //todo
 }
--- src/app/shared/services/episode.service.ts
+++ src/app/shared/services/episode.service.ts
@@ -1,8 +1,34 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import { Episode } from '../models/episode.model';
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
 
 @Injectable()
 export class EpisodeService {
 
-  constructor() { }
+  episodeUrl = "http://localhost:8080/episode";
 
+  constructor(private http: HttpClient) { }
+
+  getEpisode(id: number): Observable<Episode> {
+    return this.http.get<Episode>(this.episodeUrl + "/" + id);
+  }
+
+  addEpisode(episode: Episode): Observable<Episode> {
+    return this.http.post<Episode>(this.episodeUrl, episode, httpOptions);
+  }
+
+  modifyEpisode(id: number, episode: Episode): Observable<Episode> {
+    return this.http.put<Episode>(this.episodeUrl + "/" + id, episode);
+  }
+
+  deleteEpisode(id: number): Observable<Episode> {
+    return this.http.delete<Episode>(this.episodeUrl + "/" + id);
+  }
+  //...
 }
