58dd97528f379d2d83c41c9b50faccb299a89e34
Papp Zsolt <pappzsolt3@gmail.com>
2018-10-13 05:36:14 +0200

    router order fix

--- src/app/articles/articles-routing.module.ts
+++ src/app/articles/articles-routing.module.ts
@@ -12,9 +12,9 @@ const articlesRoutes: Routes = [
     path: 'articles', component: ArticlesShellComponent,
     children: [
       { path: '', component: ArticleListComponent },
-      { path: ':id', component: ArticleComponent },
       { path: 'new-article', component: NewArticleComponent },
-      { path: 'saved-article-list', component: SavedArticleListComponent }
+      { path: 'saved-article-list', component: SavedArticleListComponent },
+      { path: ':id', component: ArticleComponent }
     ]
   }
 ];
